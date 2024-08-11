3dc11aacbdfef11d41455967e398e0b6e7759362
unknown <kareem.2512003@gmail.com>
2024-08-11 11:18:59 +0300

    add analysis

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,19 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ar">
-      <Head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-7VJ863BDFX"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-7VJ863BDFX');
-            `,
-          }}
-        />
-      </Head>
+      <Analytics></Analytics>
       <body className={inter.className}>{children}</body>
     </html>
   );
